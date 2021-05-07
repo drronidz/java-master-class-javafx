86f3aa82d51331771fdf98caa6d22896501aabf2
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-07 14:05:32 +0100

    issue done. closes #16

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -15,6 +16,9 @@ public class Controller {
                     helloButton,
                     byeButton;
 
+    @FXML
+    private CheckBox ourCheckBox;
+
 
     @FXML
     public void initialize() {
@@ -24,11 +28,23 @@ public class Controller {
 
     @FXML
     public void onButtonClicked (ActionEvent event){
+
         System.out.println("Hello, " + nameField.getText());
         if(event.getSource().equals(helloButton)) {
             System.out.println("Hello, " + nameField.getText());
         } else if(event.getSource().equals(byeButton)) {
             System.out.println("Bye, " + nameField.getText());
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException interruptedException) {
+            // we don't care bout this
+        }
+        if(ourCheckBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
         }
     }
 
@@ -38,5 +54,10 @@ public class Controller {
         boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+
+    @FXML
+    public void handleChange() {
+        System.out.println("The CheckBox is " + (ourCheckBox.isSelected() ? "checked" : "not checked"));
     }
 }
