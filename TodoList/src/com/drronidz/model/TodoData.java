96623585eea2e8b34870c3b4f582ad2f5661c0c9
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-07 16:28:04 +0100

    issue done. closes #22

---
+++ TodoList/src/com/drronidz/model/TodoData.java
@@ -0,0 +1,87 @@
+package com.drronidz.model;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 5/7/2021 , 
+    CREATED ON : 4:11 PM
+*/
+
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
+
+public class TodoData {
+    private static TodoData instance = new TodoData();
+    private static String fileName = "TodoListItems.txt";
+
+    private List<TodoItem> todoItems;
+    private DateTimeFormatter dateTimeFormatter;
+
+
+    private TodoData() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void setTodoItems(List<TodoItem> todoItems) {
+        this.todoItems = todoItems;
+    }
+
+    public static TodoData getInstance() {
+        return instance;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(fileName);
+        BufferedReader bufferedReader = Files.newBufferedReader(path);
+
+        String input;
+
+        try {
+            while((input = bufferedReader.readLine() )!= null) {
+                String [] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dataString = itemPieces[3];
+
+                LocalDate date = LocalDate.parse(dataString, dateTimeFormatter);
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+            }
+        } finally {
+            bufferedReader.close();
+        }
+    }
+
+    public void storeTodoItems() throws IOException {
+        Path path = Paths.get(fileName);
+        BufferedWriter bufferedWriter = Files.newBufferedWriter(path);
+        try {
+            Iterator<TodoItem> itemIterator = todoItems.iterator();
+            while (itemIterator.hasNext()) {
+                TodoItem item = itemIterator.next();
+                bufferedWriter.write(String.format("%s\t%S\t%s",
+                        item.getShortDescription(),
+                        item.getDetails(),
+                        item.getDeadline().format(dateTimeFormatter)));
+            }
+        } finally {
+            if(bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+        }
+    }
+}
