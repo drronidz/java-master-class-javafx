3b3b222921c040133a39cb37e6b366dd766ecb8a
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-10 01:35:18 +0100

    issue done. closes #35

--- JavaFX Application/src/sample/Controller.java
+++ JavaFX Application/src/sample/Controller.java
@@ -7,11 +7,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -21,6 +29,9 @@ public class Controller implements Initializable {
 
     @FXML
     public GridPane gridPane;
+
+    @FXML
+    public WebView webView;
 
     @FXML
     private Button buttonFour;
@@ -44,14 +55,40 @@ public class Controller implements Initializable {
 
     @FXML
     public void handleClick(ActionEvent event) {
-//        FileChooser fileChooser = new FileChooser();
+
+        FileChooser fileChooser = new FileChooser();
+        //fileChooser.setTitle("Save Application File");
+
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Zip","*.zip"),
+                new FileChooser.ExtensionFilter("PDF","*.pdf"),
+                new FileChooser.ExtensionFilter("Image Files","*.jpg","*.png","*.gif"),
+                new FileChooser.ExtensionFilter("All Files","*.*")
+        );
+
 //        fileChooser.showOpenDialog(gridPane.getScene().getWindow());
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        File file = directoryChooser.showDialog(gridPane.getScene().getWindow());
-        if(file != null) {
-            System.out.println(file.getPath());
+//        DirectoryChooser directoryChooser = new DirectoryChooser();
+
+        List<File> files = fileChooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+        if(files != null) {
+            for (File value : files) {
+                System.out.println(value);
+            }
+            //System.out.println(files.getPath());
         } else {
             System.out.println("Chooser was cancelled");
         }
+    }
+
+    @FXML
+    public void handleLinkClick(ActionEvent event) {
+//        System.out.println("The link was clicked");
+//        try {
+//            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//        } catch (IOException | URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load("http://www.javafx.com");
     }
 }
