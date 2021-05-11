42d417475995152f40822931134b049b61c54245
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-05-11 01:14:25 +0100

    issue done. closes #43

---
+++ JavaFxChallenge/src/com/ContactController.java
@@ -0,0 +1,25 @@
+package com;/*
+    CREATED BY : ABD EL HALIM
+    PROJECT NAME : {IntelliJ IDEA}
+    CREATED ON : 5/11/2021 , 
+    CREATED ON : 1:00 AM
+*/
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ContactController {
+
+    @FXML
+    public TextField firstNameField;
+
+    @FXML
+    public TextField lastNameField;
+
+    @FXML
+    public TextField phoneNumberField;
+
+    @FXML
+    public TextField notesField;
+
+}
